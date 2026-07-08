d53578a4689b6b817a3acca653a2d7f69f1df9d1
tillplusplus <tillpp@users.noreply.github.com>
2026-07-08 09:18:38 +0200

    improve smartphone view, update background

--- frontEnd/src/App.vue
+++ frontEnd/src/App.vue
@@ -1,35 +1,16 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
-import { setRandomText } from "./motd_randomiser"
-
-
-const message = ref("You did it!")
-onMounted(async()=>{
-  try {
-    const res = await fetch("http://localhost:3000/test/1")
-    const data = await res.json()
-    message.value = data.message
-  } catch (error) {
-    console.log(error)
-    message.value = String(error)
-  }
-  setRandomText();
-})
 </script>
 
 <template>
       <nav>
       <div class="banner">
-          <img class="logo" src="/assets/images/CubyzLogo.png" alt="">
-          <br>
-
-          <p id="motd-text">
-              "Why is nothing here?" - you (probably)
-          </p>          
+        <img class="logo" src="/assets/images/CubyzLogo.png" alt="">
+        <div>
           <router-link to="."><div class="button">Home</div></router-link> 
           <router-link to="."><div class="button">Features (no page)</div></router-link> 
           <router-link to="information"><div class="button">Information</div></router-link> 
           <router-link to="addon"><div class="button">Addon</div></router-link> 
+        </div>
       </div>
   </nav>
   <main>
